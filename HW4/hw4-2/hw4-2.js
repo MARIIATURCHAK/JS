f8e89c4326ffd249016db05f75eadba3ca937522
MARIIATURCHAK <mariia8v@gmail.com>
2025-08-29 18:47:46 +0300

    hw4-2

---
+++ HW4/hw4-2/hw4-2.js
@@ -0,0 +1,11 @@
+// створити функцію, яка обчислює та повертає площу кола з радіусом r
+function rectangleArea(radius) {
+    return Math.PI * radius * radius;
+}
+rectangleArea()
+
+
+
+
+// let result = rectangleArea(5);
+// console.log(result);
