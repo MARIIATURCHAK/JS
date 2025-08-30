8e06690d1a755ea3e70be81ef349dff600a15f4b
MARIIATURCHAK <mariia8v@gmail.com>
2025-08-30 23:45:28 +0300

    hw4-7 and hw4-8

--- HW4/hw4-7/hw4-7.js
+++ HW4/hw4-7/hw4-7.js
@@ -5,8 +5,8 @@
 function list(text, count){
         document.write('<ul>');
     for (let i = 0; i < count; i++) {
-        document.write(`<li>${text}</li>`)
+        document.write(`<li>${text}</li>`);
     }
-        document.write('</ul>')
+        document.write('</ul>');
 }
 list();
