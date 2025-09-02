7367d080551588bb2a1990793f2f26c610f06593
MARIIATURCHAK <mariia8v@gmail.com>
2025-09-03 00:49:13 +0300

    hw5.1-5.10

---
+++ HW5/hw5-10/hw5-10.js
@@ -0,0 +1,18 @@
+// – створити функцію, яка повертає найменше число з масиву
+
+let arrayMinValue = (numbers) => {
+    let min = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        const number = numbers[i];
+        if (number < min) {
+            min = number;
+        }
+    }
+    return min;
+}
+console.log(arrayMinValue([10, -5, 10, 99, -55]))
+
+
+
+
+
